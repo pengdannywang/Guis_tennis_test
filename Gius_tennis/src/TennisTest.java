c92e1aa5c02b31c3d235defc840e26a2ce337c57
pengwang <pengdannywang@gmail.com>
2019-12-17 12:32:35 +1100

    test

--- Gius_tennis/src/TennisTest.java
+++ Gius_tennis/src/TennisTest.java
@@ -1,9 +1,7 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 import guis.tennis.Match;
 
 /**
